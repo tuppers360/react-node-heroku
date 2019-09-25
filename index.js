79091e90cacc55963e9db84838124bceba1b6746
tuppers360 <tuppers360@live.co.uk>
2019-09-25 08:04:46 +0100

    intital commit 2

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  const user = req.query.user || "tuppers360";
+  axios.get(`https://api.github.com/users/${user}`).then(response => {
+    res.json({ user: response.data });
+  });
+});
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
